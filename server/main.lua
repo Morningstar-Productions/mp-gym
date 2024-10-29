d061138df96303db1202bec170d9f912bf3110b9
xViperAG <usairsoftingandgaming@gmail.com>
2024-10-29 16:22:34 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,8 +1,9 @@
 local config = require 'config.shared'
+local QBX = exports.qbx_core
 
 lib.callback.register('mp-gym:server:givePass', function(source)
     local src = source
-    local player = exports.qbx_core:GetPlayer(src)
+    local player = QBX:GetPlayer(src)
 
     if player.Functions.RemoveMoney('cash', config.gymPass.ptem) then
         exports.ox_inventory:AddItem(src, config.gymPass.item, 1)
@@ -13,7 +14,7 @@ lib.callback.register('mp-gym:server:givePass', function(source)
 end)
 
 lib.callback.register('mp-gym:server:hasMoney', function(source)
-    local player = exports.qbx_core:GetPlayer(source)
+    local player = QBX:GetPlayer(source)
     local playerMoney = player.PlayerData.money.cash
 
     if playerMoney ~= config.gymPass.price then
