7f1c3fccf40e8733dac52abbdb2912dac48af17b
xViperAG <82969741+xViperAG@users.noreply.github.com>
2025-01-14 11:16:38 -0500

    Multiframework Support

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,9 @@
 local config = require 'config.shared'
-local QBX = exports.qbx_core
 
 lib.callback.register('mp-gym:server:givePass', function(source)
     local src = source
-    local player = QBX:GetPlayer(src)
 
-    if player.Functions.RemoveMoney('cash', config.gymPass.ptem) then
+    if Renewed.removeMoney(src, 'cash', config.gymPass.price) then
         exports.ox_inventory:AddItem(src, config.gymPass.item, 1)
         return true
     end
@@ -14,8 +12,7 @@ lib.callback.register('mp-gym:server:givePass', function(source)
 end)
 
 lib.callback.register('mp-gym:server:hasMoney', function(source)
-    local player = QBX:GetPlayer(source)
-    local playerMoney = player.PlayerData.money.cash
+    local playerMoney = Renewed.getMoney(source, 'cash')
 
     if playerMoney ~= config.gymPass.price then
         return false
